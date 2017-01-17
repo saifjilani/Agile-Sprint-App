5ee7ef80a09ed45a457eed8c1f886546f2e9bf21
Saif Jilani <saifali@umich.edu>
2017-01-16 23:07:14 -0500

    add validations

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,4 +2,10 @@ class Sprint < ApplicationRecord
   has_many :features
   has_many :sprint_users
   has_many :users, through: :sprint_users
+
+  validates_associated :features
+  validates_associated :sprint_users
+  validates_associated :users
+  validates_length_of :title, maximum: 50
+  validates_presence_of :title, :start_date, :end_date
 end
--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -6,4 +6,45 @@ class FeatureTest < ActiveSupport::TestCase
     feature = features(:regular_feature)
     assert_equal sprint.id, feature.sprint.id
   end
+
+  test 'validates belongs to sprint' do
+    feature = Feature.new
+    refute feature.valid?
+  end
+
+  test 'is valid' do
+    feature = features(:regular_feature)
+    assert feature.valid?
+  end
+
+  test 'validates title length' do
+    feature = features(:regular_feature)
+    title_greater_than_50 = (0...51).map { ('a'..'z').to_a[rand(26)] }.join
+    feature.title = title_greater_than_50
+    refute feature.valid?
+  end
+
+  test 'validates presence of rank' do
+    feature = features(:regular_feature)
+    feature.rank = nil
+    refute feature.valid?
+  end
+
+  test 'validates presence of estimated_total_hours' do
+    feature = features(:regular_feature)
+    feature.estimated_total_hours = nil
+    refute feature.valid?
+  end
+
+  test 'validates presence of estimated_remaining_hours' do
+    feature = features(:regular_feature)
+    feature.estimated_remaining_hours = nil
+    refute feature.valid?
+  end
+
+  test 'validates presence of actual_worked_hours' do
+    feature = features(:regular_feature)
+    feature.actual_worked_hours = nil
+    refute feature.valid?
+  end
 end
